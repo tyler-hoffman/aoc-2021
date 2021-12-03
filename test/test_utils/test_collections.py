81a7f015b810baff4ba4c38f6eff4840ebd7bcc5
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-03 08:53:11 -0500

    Test frequency_map

---
+++ test/test_utils/test_collections.py
@@ -0,0 +1,17 @@
+from typing import Dict, List
+import unittest
+from parameterized import parameterized
+
+from src.utils.collections import frequency_map
+
+
+class TestFrequencyMap(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ([], {}),
+            (["a"], {"a": 1}),
+            (["a", "b", "a"], {"a": 2, "b": 1}),
+        ]
+    )
+    def test_sliding_window(self, input: List[str], expected: Dict[str, int]):
+        self.assertEqual(frequency_map(input), expected)

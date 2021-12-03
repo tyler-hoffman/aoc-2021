cb5410298fa89bb66cd3f03aa93422b8c15979b2
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-03 08:45:35 -0500

    Minor refactor of 03.b

--- src/day_03/b.py
+++ src/day_03/b.py
@@ -8,21 +8,30 @@ from src.utils.collections import frequency_map
 def solve(input: str) -> int:
     lines = parse(input)
 
-    oxygen_generator_rating = OxygenGeneratorRatingFinder().filter_lines(lines)
-    c02_scrubber_rating = C02ScrubberRatingFinder().filter_lines(lines)
+    o2_rating = OxygenGeneratorRatingFinder().find_rating(lines)
+    c02_rating = C02ScrubberRatingFinder().find_rating(lines)
 
-    return int(oxygen_generator_rating, 2) * int(c02_scrubber_rating, 2)
+    return o2_rating * c02_rating
 
 
 class RatingFinder(ABC):
+    def find_rating(self, lines: List[str]) -> int:
+        rating_string = self.filter_lines(lines)
+        return int(rating_string, 2)
+
     def filter_lines(self, lines: List[str]) -> str:
         line_len = len(lines[0])
         winning_bit: str = ""
         for index in range(line_len):
             if len(lines) == 1:
                 break
+
             freqs = frequency_map([line[index] for line in lines])
-            winning_bit = self.select_winning_bit(freqs)
+            zero_count = freqs.get("0", 0)
+            one_count = freqs.get("1", 0)
+            winning_bit = self.select_winning_bit(
+                zero_count=zero_count, one_count=one_count
+            )
 
             lines = [line for line in lines if line[index] == winning_bit]
 
@@ -30,23 +39,17 @@ class RatingFinder(ABC):
         return lines[0]
 
     @abstractmethod
-    def select_winning_bit(freqs: Dict[str, int]) -> str:
+    def select_winning_bit(self, zero_count, one_count: int) -> str:
         ...
 
 
 class OxygenGeneratorRatingFinder(RatingFinder):
-    def select_winning_bit(self, freqs: Dict[str, int]) -> str:
-        zero_count = freqs.get("0", 0)
-        one_count = freqs.get("1", 0)
-
+    def select_winning_bit(self, zero_count, one_count: int) -> str:
         return "1" if one_count >= zero_count else "0"
 
 
 class C02ScrubberRatingFinder(RatingFinder):
-    def select_winning_bit(self, freqs: Dict[str, int]) -> str:
-        zero_count = freqs.get("0", 0)
-        one_count = freqs.get("1", 0)
-
+    def select_winning_bit(self, zero_count, one_count: int) -> str:
         return "0" if zero_count <= one_count else "1"
 
 

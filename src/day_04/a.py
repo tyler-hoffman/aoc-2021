0f326dde91f2fa620eb127df9be2a3418df45fd9
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-04 09:14:12 -0500

    Solve 04.a

--- src/day_04/a.py
+++ src/day_04/a.py
@@ -1,5 +1,19 @@
+
+from typing import List, Set
+from src.day_04.shared import parse
+
+
 def solve(input: str) -> int:
-    return -1
+    data = parse(input)
+
+    to_check: Set[int] = set()
+    for item in data.picks:
+        to_check.add(item)
+
+        for board in data.boards:
+            if board.has_match(to_check):
+                unmarked = board.unmarked_stuff(to_check)
+                return sum(unmarked) * item
 
 
 if __name__ == "__main__":

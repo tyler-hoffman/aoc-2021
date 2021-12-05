e60611e6dec083f47f96dce004bfb02d593b387a
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-05 08:43:15 -0500

    Backfill solution test

--- test/test_day_05/test_b.py
+++ test/test_day_05/test_b.py
@@ -20,3 +20,8 @@ SAMPLE_DATA = """
 class TestDay05B(unittest.TestCase):
     def test_solve(self):
         self.assertEqual(solve(SAMPLE_DATA), 12)
+
+    def test_solution(self):
+        with open("src/day_05/input.txt", "r") as f:
+            input = f.read()
+        self.assertEqual(solve(input), 21514)

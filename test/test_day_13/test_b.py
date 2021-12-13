5f79be43a5f80b6502f52d29bc88a9472cd848bf
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-13 13:40:43 -0500

    Rework test

--- test/test_day_13/test_b.py
+++ test/test_day_13/test_b.py
@@ -6,10 +6,10 @@ from .sample_data import SAMPLE_DATA
 from src.day_13.b import solve
 
 # Sorry that the expected outputs are so ugly here!
-# I found the periods for empty space to be hard to look at
-# so I copy-and-pasted from the sample output and then replaced
-# periods with spaces, but to backfill the solution, I just
-# copy-and-pasted from my prettier output.
+# I found the periods for empty space to be hard to look at.
+# So for the sample I copy-and-pasted from the expected output
+# and then replaced # periods with spaces. To backfill the solution,
+# I just copy-and-pasted from my nicer-looking output.
 # Oh, and we have to omit the first character here because
 # it's a newline for formatting that shouldn't really be present.
 
@@ -40,22 +40,28 @@ EXPECTED_FOR_INPUT = """
 class TestDay13B(TestCase):
     """
     This one requires human input to read the characters.
-    These tests just verifies we pass the right string to the human.
+    These tests mostly just verify we pass the right string to the human.
+    It also verifies we return whatever the user said the string looked like
     """
+
+    arbitrary_human_response = "dinosaur"
 
     def test_solve(self):
         question_asker = mock.MagicMock(spec=QuestionAsker)
+        question_asker.ask.return_value = self.arbitrary_human_response
 
-        solve(SAMPLE_DATA, question_asker)
+        solution = solve(SAMPLE_DATA, question_asker)
 
         question_asker.say.assert_called_with(EXPECTED_FOR_SAMPLE)
+        self.assertEqual(solution, self.arbitrary_human_response)
 
     def test_solution(self):
+        question_asker = mock.MagicMock(spec=QuestionAsker)
+        question_asker.ask.return_value = self.arbitrary_human_response
         with open("src/day_13/input.txt", "r") as f:
             input_string = f.read()
 
-        question_asker = mock.MagicMock(spec=QuestionAsker)
-
-        solve(input_string, question_asker)
+        solution = solve(input_string, question_asker)
 
         question_asker.say.assert_called_with(EXPECTED_FOR_INPUT)
+        self.assertEqual(solution, self.arbitrary_human_response)

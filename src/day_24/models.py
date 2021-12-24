4c308045fea5766832911cd1c048e951bafde9ae
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-24 15:26:32 -0500

    Add state

--- src/day_24/models.py
+++ src/day_24/models.py
@@ -1,6 +1,42 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
+
+
+@dataclass(frozen=True)
+class State(object):
+    w: int = 0
+    x: int = 0
+    y: int = 0
+    z: int = 0
+
+    def get(self, var: str) -> int:
+        match var:
+            case "w":
+                return self.w
+            case "x":
+                return self.x
+            case "y":
+                return self.y
+            case "z":
+                return self.z
+
+    def set(self, var: str, val: int) -> State:
+        w = self.w
+        x = self.x
+        y = self.y
+        z = self.z
+        match var:
+            case "w":
+                w = val
+            case "x":
+                x = val
+            case "y":
+                y = val
+            case "z":
+                z = val
+        return State(w, x, y, z)
 
 
 class BinaryOperatorType(Enum):

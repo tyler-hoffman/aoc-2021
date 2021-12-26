3469fedbbabbc56bbd62230b37a4cd2478b2fdbb
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-25 19:45:44 -0500

    Remove unused file

--- test/test_day_24/test_a.py
+++ test/test_day_24/test_a.py
@@ -4,7 +4,7 @@ from src.day_24.a import solve
 
 
 class TestDay24A(unittest.TestCase):
-    def test_solve(self):
+    def test_solution(self):
         with open("src/day_24/input.txt", "r") as f:
             input = f.read()
             self.assertEqual(solve(input), 99995969919326)
--- test/test_day_24/test_b.py
+++ test/test_day_24/test_b.py
@@ -4,7 +4,7 @@ from src.day_24.b import solve
 
 
 class TestDay24B(unittest.TestCase):
-    def test_solve(self):
+    def test_solution(self):
         with open("src/day_24/input.txt", "r") as f:
             input = f.read()
             self.assertEqual(solve(input), 48111514719111)
